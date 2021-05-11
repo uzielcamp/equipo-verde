c4d82dd32f98d77033a8cd20a3cb4b8d29e21864
l <ana.bouchan@madero.edu.mx>
2021-05-11 04:54:34 +0200

    Codigo de Company

--- equipo-verde/Proyecto Final/Entidades/Company.cs
+++ equipo-verde/Proyecto Final/Entidades/Company.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,29 @@ namespace Proyecto_Final.Entidades
 {
     public class Company
     {
+        #region Propiedades Auto Implementadas 
+     [Key]
+     public int Id { get; set; }
+    [Required]
+    [StringLength(30)]
+    public string Name { get; set; }
+    [Required]
+    [StringLength(30)]
+    public string Address { get; set; }
+    [Required]
+    [StringLength(30)]
+    public string postalCode { get; set; }
+    [Required]
+    [StringLength(30)]
+    public string phoneNumberc { get; set; }
+    [Required]
+    [StringLength(30)]
+    public string website { get; set; }
+    [Required]
+    [StringLength(30)]
+    public string email { get; set; }
+    [StringLength(30)]
+    public string Photo { get; set; }
+        #endregion
     }
 }
