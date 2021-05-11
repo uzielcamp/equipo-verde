23f255868b3a7ac858a6f7c7be4d1cb8411d7f77
MIGUEL ANGEL HERRERA SANCHEZ <sanchezherreraangelmiguel@gmail.com>
2021-05-11 12:35:32 -0500

    Se creo entraedas para las clases de companies, major y student

--- equipo-verde/Proyecto Final/DataContext.cs
+++ equipo-verde/Proyecto Final/DataContext.cs
@@ -11,9 +11,12 @@ namespace Proyecto_Final
    public class DataContext:DbContext
     {
         public DbSet<Adviser> Advisers { get; set; }
-        public DbSet<Coach>Coaches { get; set; }
+        public DbSet<Coach> Coaches { get; set; }
+        public DbSet<Company> Comapanies { get; set; }
         public DbSet<Contact> Contacts { get; set; }
         public DbSet <Coordinator> Coordinators { get; set; }
+        public DbSet<Major> Majors { get; set; }
+        public DbSet <Student> Students { get; set; }
         public DataContext() : base("name=miConexion")
         {
 
