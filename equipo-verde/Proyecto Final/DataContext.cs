52168e4c0e3d4347dc8d7c63ce5b29367ead0ed5
Alejandro Espinoza <asedipro@hotmail.com>
2021-05-10 21:05:52 -0500

    Creacion de la base de datos

---
+++ equipo-verde/Proyecto Final/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Proyecto_Final.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final
+{
+   public class DataContext:DbContext
+    {
+    public DbSet<Contact> Contacts { get; set; }
+        public DataContext() : base("name=miConexion")
+        {
+
+        }
+
+    }
+    
+    
+}
