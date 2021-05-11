e594a3825314d1496dcdbd1de7c73d6f530393d7
MIGUEL ANGEL HERRERA SANCHEZ <sanchezherreraangelmiguel@gmail.com>
2021-05-11 11:39:29 -0500

    Se agrego codigo en la entidad de Student

--- equipo-verde/Proyecto Final/Entidades/Student.cs
+++ equipo-verde/Proyecto Final/Entidades/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,28 @@ namespace Proyecto_Final.Entidades
 {
     public class Student
     {
+        #region
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string Birthdate { get; set; }
+        [Required]
+        [StringLength(30)]
+        public string PhoneNumber { get; set; }
+        [StringLength(30)]
+        public string Cellphone { get; set; }
+        [StringLength(30)]
+        public string Email { get; set; }
+        [StringLength(30)]
+        public string Photo { get; set; }
+        public string FullName { get { return $"{FirstName}{LastName}"; } }
+        #endregion
     }
 }
