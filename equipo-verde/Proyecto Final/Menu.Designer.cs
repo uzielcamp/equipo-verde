6c29cf4185f5d1e6b9591cd8553496ed2e23c544
uzielcamp <uziel.camp776@gmail.com>
2023-04-18 21:43:15 -0600

    cambio

---
+++ equipo-verde/Proyecto Final/Menu.Designer.cs
@@ -0,0 +1,163 @@
+﻿
+namespace Proyecto_Final
+{
+    partial class frmMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bttAbirAdviser = new MetroFramework.Controls.MetroButton();
+            this.bttAbirCoach = new MetroFramework.Controls.MetroButton();
+            this.bttAbirCompany = new MetroFramework.Controls.MetroButton();
+            this.bttAbrirCoordinator = new MetroFramework.Controls.MetroButton();
+            this.bttAbirMajor = new MetroFramework.Controls.MetroButton();
+            this.bttAbrirContact = new MetroFramework.Controls.MetroButton();
+            this.bttAbirStudent = new MetroFramework.Controls.MetroButton();
+            this.lblMensajeDeMenu = new MetroFramework.Controls.MetroLabel();
+            this.SuspendLayout();
+            // 
+            // bttAbirAdviser
+            // 
+            this.bttAbirAdviser.Location = new System.Drawing.Point(49, 230);
+            this.bttAbirAdviser.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bttAbirAdviser.Name = "bttAbirAdviser";
+            this.bttAbirAdviser.Size = new System.Drawing.Size(111, 76);
+            this.bttAbirAdviser.TabIndex = 0;
+            this.bttAbirAdviser.Text = "Ir a Adviser";
+            this.bttAbirAdviser.UseSelectable = true;
+            this.bttAbirAdviser.Click += new System.EventHandler(this.bttAbirAdviser_Click);
+            // 
+            // bttAbirCoach
+            // 
+            this.bttAbirCoach.Location = new System.Drawing.Point(168, 230);
+            this.bttAbirCoach.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bttAbirCoach.Name = "bttAbirCoach";
+            this.bttAbirCoach.Size = new System.Drawing.Size(111, 76);
+            this.bttAbirCoach.TabIndex = 1;
+            this.bttAbirCoach.Text = "Ir a Coach";
+            this.bttAbirCoach.UseSelectable = true;
+            this.bttAbirCoach.Click += new System.EventHandler(this.bttAbirCoach_Click);
+            // 
+            // bttAbirCompany
+            // 
+            this.bttAbirCompany.Location = new System.Drawing.Point(287, 230);
+            this.bttAbirCompany.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bttAbirCompany.Name = "bttAbirCompany";
+            this.bttAbirCompany.Size = new System.Drawing.Size(111, 76);
+            this.bttAbirCompany.TabIndex = 2;
+            this.bttAbirCompany.Text = "Ir a Company";
+            this.bttAbirCompany.UseSelectable = true;
+            this.bttAbirCompany.Click += new System.EventHandler(this.bttAbirCompany_Click);
+            // 
+            // bttAbrirCoordinator
+            // 
+            this.bttAbrirCoordinator.Location = new System.Drawing.Point(524, 230);
+            this.bttAbrirCoordinator.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bttAbrirCoordinator.Name = "bttAbrirCoordinator";
+            this.bttAbrirCoordinator.Size = new System.Drawing.Size(123, 76);
+            this.bttAbrirCoordinator.TabIndex = 3;
+            this.bttAbrirCoordinator.Text = "Ir a Coordinator";
+            this.bttAbrirCoordinator.UseSelectable = true;
+            this.bttAbrirCoordinator.Click += new System.EventHandler(this.bttAbrirCoordinator_Click);
+            // 
+            // bttAbirMajor
+            // 
+            this.bttAbirMajor.Location = new System.Drawing.Point(655, 230);
+            this.bttAbirMajor.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bttAbirMajor.Name = "bttAbirMajor";
+            this.bttAbirMajor.Size = new System.Drawing.Size(111, 76);
+            this.bttAbirMajor.TabIndex = 4;
+            this.bttAbirMajor.Text = "Ir a Major";
+            this.bttAbirMajor.UseSelectable = true;
+            this.bttAbirMajor.Click += new System.EventHandler(this.bttAbirMajor_Click);
+            // 
+            // bttAbrirContact
+            // 
+            this.bttAbrirContact.Location = new System.Drawing.Point(405, 230);
+            this.bttAbrirContact.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bttAbrirContact.Name = "bttAbrirContact";
+            this.bttAbrirContact.Size = new System.Drawing.Size(111, 76);
+            this.bttAbrirContact.TabIndex = 5;
+            this.bttAbrirContact.Text = "Ir a Contact";
+            this.bttAbrirContact.UseSelectable = true;
+            this.bttAbrirContact.Click += new System.EventHandler(this.bttAbrirContact_Click);
+            // 
+            // bttAbirStudent
+            // 
+            this.bttAbirStudent.Location = new System.Drawing.Point(773, 230);
+            this.bttAbirStudent.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.bttAbirStudent.Name = "bttAbirStudent";
+            this.bttAbirStudent.Size = new System.Drawing.Size(111, 76);
+            this.bttAbirStudent.TabIndex = 6;
+            this.bttAbirStudent.Text = "Ir a  Student";
+            this.bttAbirStudent.UseSelectable = true;
+            this.bttAbirStudent.Click += new System.EventHandler(this.bttAbirStudent_Click);
+            // 
+            // lblMensajeDeMenu
+            // 
+            this.lblMensajeDeMenu.AutoSize = true;
+            this.lblMensajeDeMenu.FontSize = MetroFramework.MetroLabelSize.Tall;
+            this.lblMensajeDeMenu.Location = new System.Drawing.Point(361, 151);
+            this.lblMensajeDeMenu.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMensajeDeMenu.Name = "lblMensajeDeMenu";
+            this.lblMensajeDeMenu.Size = new System.Drawing.Size(150, 25);
+            this.lblMensajeDeMenu.TabIndex = 7;
+            this.lblMensajeDeMenu.Text = "¿Donde desea ir?";
+            // 
+            // frmMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(899, 337);
+            this.Controls.Add(this.lblMensajeDeMenu);
+            this.Controls.Add(this.bttAbirStudent);
+            this.Controls.Add(this.bttAbrirContact);
+            this.Controls.Add(this.bttAbirMajor);
+            this.Controls.Add(this.bttAbrirCoordinator);
+            this.Controls.Add(this.bttAbirCompany);
+            this.Controls.Add(this.bttAbirCoach);
+            this.Controls.Add(this.bttAbirAdviser);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmMenu";
+            this.Padding = new System.Windows.Forms.Padding(27, 74, 27, 25);
+            this.Text = "Menu";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MetroFramework.Controls.MetroButton bttAbirAdviser;
+        private MetroFramework.Controls.MetroButton bttAbirCoach;
+        private MetroFramework.Controls.MetroButton bttAbirCompany;
+        private MetroFramework.Controls.MetroButton bttAbrirCoordinator;
+        private MetroFramework.Controls.MetroButton bttAbirMajor;
+        private MetroFramework.Controls.MetroButton bttAbrirContact;
+        private MetroFramework.Controls.MetroButton bttAbirStudent;
+        private MetroFramework.Controls.MetroLabel lblMensajeDeMenu;
+    }
+}
---
+++ equipo-verde/Proyecto Final/Menu.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Proyecto_Final
+{
+    public partial class frmMenu : MetroFramework.Forms.MetroForm
+    {
+        public frmMenu()
+        {
+            InitializeComponent();
+        }
+
+        private void bttAbirAdviser_Click(object sender, EventArgs e)
+        {
+            frmAdviser frmAdviser = new frmAdviser();
+            frmAdviser.Show();
+        }
+
+        private void bttAbirCoach_Click(object sender, EventArgs e)
+        {
+            frmCoach frmCoach = new frmCoach();
+            frmCoach.Show();
+        }
+
+        private void bttAbirCompany_Click(object sender, EventArgs e)
+        {
+            frmCompany frmCompany = new frmCompany();
+            frmCompany.Show();
+        }
+
+        private void bttAbrirContact_Click(object sender, EventArgs e)
+        {
+            frmContact frmContact = new frmContact();
+            frmContact.Show();
+        }
+
+        private void bttAbrirCoordinator_Click(object sender, EventArgs e)
+        {
+            frmCoordinator frmCoordinator = new frmCoordinator();
+            frmCoordinator.Show();
+        }
+
+        private void bttAbirMajor_Click(object sender, EventArgs e)
+        {
+            frmMajor frmMajor = new frmMajor();
+            frmMajor.Show();
+        }
+
+        private void bttAbirStudent_Click(object sender, EventArgs e)
+        {
+            frmStudent frmStudent = new frmStudent();
+            frmStudent.Show();
+        }
+    }
+}
